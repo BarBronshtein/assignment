38b235030c61db42a0daf082080c40b6c47cd329
Bnaya <bnaya98@gmail.com>
2022-08-10 16:55:44 +0300

    fix

--- frontend/src/components/card.vue
+++ frontend/src/components/card.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="icon-card flex flex-column items-center">
 		<div class="img-container">
-			<img :src="product.src" />
+			<img :src="product.imgSRC" />
 		</div>
 		<h3>Price:${{ product.price }}</h3>
 		<p>{{ product.title }}</p>
